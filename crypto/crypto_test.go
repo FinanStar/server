a81dc2d4822837325c92ed17c1d6c087e3fb8bf7
Andy <wampirchikkkk@yandex.ru>
2024-12-17 22:39:06 +0500

    test: [crypto] testing crypto methods

---
+++ crypto/crypto_test.go
@@ -0,0 +1,39 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateSecureId(t *testing.T) {
+	require := require.New(t)
+
+	id, err := GenerateSecureId(16)
+
+	require.Nil(err)
+	require.Equal(len(id), 32)
+}
+
+func TestHashPassword(t *testing.T) {
+	require := require.New(t)
+
+	hashedPassword, err := HashPassword(`secure-password`)
+
+	require.Nil(err)
+	require.Contains(hashedPassword, `$argon2id$v=19$m=19456,t=2,p=1$`)
+}
+
+func TestComparePasswords(t *testing.T) {
+	require := require.New(t)
+
+	originalPassword := `secure-password`
+	hashedPassword, err := HashPassword(originalPassword)
+
+	require.Nil(err)
+
+	match, err := ComparePasswords(originalPassword, hashedPassword)
+
+	require.Nil(err)
+	require.Equal(match, true)
+}
